7517dfd82fcaad8d4d4f8f9a0858adad149f2496
ecSanders <ecsander0@gmail.com>
2021-05-01 11:42:37 -0600

    Solved vowel counter problem

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,1 +1,10 @@
+def vowel_counter(my_string):
+    my_string = my_string.lower()
+    vowels = ['a','e','i','o','u']
+    counter = 0
+    for i in my_string:
+        if i in vowels:
+            counter += 1
+    return counter
 
+vowel_counter('This is my string')
