65014c0530cd6bc92955819d86a30a7c60696848
ecSanders <ecsander0@gmail.com>
2021-05-01 11:26:02 -0600

    Solved largest divisor problem

--- largest_divisor.py
+++ largest_divisor.py
@@ -1,19 +1,11 @@
-def greatest_product(my_list):
-    num1 = 0
-    num2 = 0
-    product = 0
+def largest_divisor(n):
+    largest_div = 0
+    if n % 2 == 0:
+        return n /2
+    
+    for i in range(1,n):
+        if n % i == 0:
+            largest_div = i
+            print(largest_div)
 
-    for i in my_list:
-        if i > num1:
-            num1 = i
-    for i in my_list:
-        if i > num2 and i < num1:
-            num2 = i
-    print(f'num1: {num1} \nnum2: {num2}')
-
-    product = num1 * num2    
-
-    return product
-
-
-print(negative([6,3,7,1,2]))
+largest_divisor(21)
