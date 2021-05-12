06f77e9d9f95cb5fda197b7b2a4a9ef83ece3b35
Erik Sanders <san17015@byui.edu>
2021-05-11 20:26:51 -0600

    Got rid of duplicate

--- runtime_BS_LS.py
+++ runtime_BS_LS.py
@@ -12,47 +12,7 @@ import time
 
 # Set up
 my_list = []
-for i in range(1000000):
-  my_list.append(random.randint(1,10000))
-
-# Linear Search
-def linear_search(my_list, target):
-  for i in my_list:
-    if i == target:
-      return True
-  return False
-
-# Binary Search
-def binary_search(my_list, target):
-
-  mid = len(my_list)//2
-
-  if my_list[mid] == target:
-    return True
-  
-  if len(my_list) == 1:
-    return False
-
-  if my_list[mid] > target:
-    return binary_search(my_list[:mid], target)
-  else:
-    return binary_search(my_list[mid:], target)
-
-my_list.sort()
-
-
-l_start = time.time()
 for i in range(1000):
-  target = random.randint(1,10000)
-  linear_search(my_list, target)
-l_end = time.time()
-
-import random
-import time
-
-# Set up
-my_list = []
-for i in range(10000):
   my_list.append(random.randint(1,10000))
 
 # Linear Search
@@ -90,6 +50,10 @@ for i in range(1000):
   target = random.randint(1,10000)
   binary_search(my_list, target)
 b_end = time.time()
+linear_time = l_end - l_start
+binary_time = b_end - b_start
 
-print(f'Linear Search: {l_end - l_start}')
-print(f'Binary Search: {b_end - b_start}')
+print(f'Linear Search: {linear_time}')
+print(f'Binary Search: {binary_time}')
+
+print(f'Binary search is {linear_time - binary_time:.2f} seconds faster')
