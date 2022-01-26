b8ef196a7abdc1613a2bea1ec424b188d61486bc
ecSanders <ecsander0@gmail.com>
2022-01-26 13:09:51 -0700

    messed around with thresh and contour

--- Python Practice/Computer Vision/calculating_points.py
+++ Python Practice/Computer Vision/calculating_points.py
@@ -9,7 +9,25 @@ data = pickle.load(pkl)
 
 img = cv.imread('mrcnn_pred.png')
 #%% Display image
+# cv.imshow('mrcnn_pred.png', img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+# %% Get contours
+imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ret, thresh = cv.threshold(imgray, 145, 255, cv.THRESH_BINARY)
+contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+#%% Draw contours
+cnt = contours[4]
+cv.drawContours(img, contours, -1, (0,255,0), 2)
+
+#%% 
 cv.imshow('mrcnn_pred.png', img)
 cv.waitKey(0)
 cv.destroyAllWindows()
-# %% 
+
+
+# %% Find perfect contour
+
+
+# %%
