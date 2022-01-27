c29247b9c178c861d38beb72bba57f3dc266ad43
ecSanders <ecsander0@gmail.com>
2022-01-27 11:22:44 -0700

    na

--- Python Practice/Computer Vision/center_contour.py
+++ Python Practice/Computer Vision/center_contour.py
@@ -19,4 +19,15 @@ cv.imshow('mrcnn_pred.png', thresh)
 cv.waitKey(0)
 cv.destroyAllWindows()
 #%%
+# detect the contours on the binary image using cv2.CHAIN_APPROX_NONE
+contours, hierarchy = cv.findContours(image=thresh, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_NONE)
+# draw contours on the original image
+image_copy = img.copy()
+cv.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv.LINE_AA)
+# see the results
+cv.imshow('None approximation', image_copy)
+cv.waitKey(0)
+cv.imwrite('contours_none_image1.jpg', image_copy)
+cv.destroyAllWindows()
 
+# %%
