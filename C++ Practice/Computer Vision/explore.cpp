92ba3afe2106896e55ef4683dc83b0eb5d249a36
ecSanders <ecsander0@gmail.com>
2022-01-29 00:04:56 -0700

    Documentation

--- C++ Practice/Computer Vision/explore.cpp
+++ C++ Practice/Computer Vision/explore.cpp
@@ -1,3 +1,13 @@
+/*******************************************************************************************
+ * @author Erik Sanders
+ * @date 1/29/2022
+ * @details A file to experiment with the openCV library to validate installation when well
+ * Status: In progress
+ * *****************************************************************************************/
+
+
+
+
 // #include "opencv2/imgcodecs.hpp"
 // #include "opencv2/highgui.hpp"
 // #include "opencv2/imgproc.hpp"
